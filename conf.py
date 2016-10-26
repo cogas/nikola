8b794bd4e56e8c6734514a7b01caef04b89731cb
cogas <cheeky.chic.chick@gmail.com>
2016-10-26 16:28:53 +0900

    Nikola auto commit.
    Nikola version: 7.8.1

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "iuk"  # (translatable)
 BLOG_TITLE = "iuk site"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://example.com/"
+SITE_URL = "https://cogas.github.io/nikola/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
